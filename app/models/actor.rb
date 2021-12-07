153a166a1c7143ffcb7c61b3244c3bc9f4f8a317
Mitchell <Hmitchell.hall@gmail.com>
2021-12-07 11:42:59 -0600

    commit tree c1cfe380a32cbf95b3339e31c1d74bc1e1cd9e17

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
+require "pry"
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+        char_name = ''
+        char_role = ''
+     self.characters.all.map do |character|
+        char_name << character.name
+     end
+     self.shows.all.map do |show|
+        char_role << show.name
+     end
+     "#{char_name} - #{char_role}"
+    end
 end
